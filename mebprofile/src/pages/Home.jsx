d6269fa4aa0b688e6f360132da3fb1f60efd85fa
MartinBradburyPC <you@example.com>
2024-10-11 22:16:34 +0100

    react installed using vite, home page set up

---
+++ mebprofile/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Martin Bradbury</h1>
+        <h6>Page Under Construction</h6>
+
+
+
+      </div>
+    )
+  }
+}
+
+export default Home
