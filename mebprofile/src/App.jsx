835ed8f2f84dce21c44c3127835ae231e85b4400
MartinBradburyPC <you@example.com>
2024-10-13 18:03:47 +0100

    Installed React Bootstrap, BrowserRouter and routed home and about page.

--- mebprofile/src/App.jsx
+++ mebprofile/src/App.jsx
@@ -1,14 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './pages/Home'
+import About from '../pages/About'
+import {BrowserRouter, Route, Routes } from 'react-router-dom'
+
 
 function App() {
 
   return (
     <>
-      <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
