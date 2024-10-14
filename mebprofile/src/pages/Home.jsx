05e4a07c1fa26cf84d779d9159fc26146de8d4d7
MartinBradburyPC <you@example.com>
2024-10-14 17:19:31 +0100

    added a transition effect to about button on project card. Removed some unused css

--- mebprofile/src/pages/Home.jsx
+++ mebprofile/src/pages/Home.jsx
@@ -1,9 +1,9 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import styles from "../styles/Home.module.css";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import video from "../assets/trihub.mp4";
-
+import Collapse from "react-bootstrap/Collapse";
 
 const Home = () => {
   const videoRef = useRef(null); // Create a ref for the video element
@@ -17,6 +17,8 @@ const Home = () => {
     videoRef.current.currentTime = 0; // Reset video to start
   };
 
+  const [open, setOpen] = useState(false);
+
   return (
     <div className={styles.bg}>
       <div className={styles.cardPos}>
@@ -24,7 +26,6 @@ const Home = () => {
           className={styles.card}
           onMouseEnter={handleMouseEnter}
           onMouseLeave={handleMouseLeave}
-
         >
           {/* <Card.Img variant="top" src={image1} className={styles.cardbg}/> */}
           <video
@@ -42,10 +43,54 @@ const Home = () => {
           <Card.Body className={styles.cardBody}>
             <Card.Title>Tri Hub</Card.Title>
             <Card.Text>
-              A Full Stack project using React.js front end and DjangoREST back end.
+              A Full Stack project using React.js front end and DjangoREST back
+              end.
             </Card.Text>
-            <Button className={styles.btn}>Link to Project</Button>
-            <Button className={styles.btn}>About</Button>
+            <Button
+              className={styles.btn}
+            >
+              Link to Project
+            </Button>
+            <Button
+              className={styles.btn}
+              onClick={() => setOpen(!open)}
+              aria-controls="example-collapse-text"
+              aria-expanded={open}
+            >
+              About
+            </Button>
+            <div style={{ minHeight: "350px" }}>
+              <Collapse in={open}>
+                <div
+                  id="about-project"
+                  style={{
+                    position: "absolute",
+                    top: "0",
+                    left: "0",
+                    right: "0",
+                    zIndex: 1,
+                  }}
+                >
+                  <Card
+                    body
+                    style={{ backgroundColor: "rgba(255, 255, 255, 0.9)" }}
+                  >
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life
+                    accusamus terry richardson ad squid. Nihil anim keffiyeh
+                    helvetica, craft beer labore wes anderson cred nesciunt
+                    sapiente ea proident.
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life
+                    accusamus terry richardson ad squid. Nihil anim keffiyeh
+                    helvetica, craft beer labore wes anderson cred nesciunt
+                    sapiente ea proident.
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life
+                    accusamus terry richardson ad squid. Nihil anim keffiyeh
+                    helvetica, craft beer labore wes anderson cred nesciunt
+                    sapiente ea proident.
+                  </Card>
+                </div>
+              </Collapse>
+            </div>
           </Card.Body>
         </Card>
 
@@ -54,8 +99,8 @@ const Home = () => {
           <Card.Body>
             <Card.Title>Card Title</Card.Title>
             <Card.Text>
-              Some quick example text to build on the card title and make up
-              the bulk of the card's content.
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
             </Card.Text>
             <Button variant="primary">Go somewhere</Button>
           </Card.Body>
@@ -66,8 +111,8 @@ const Home = () => {
           <Card.Body>
             <Card.Title>Card Title</Card.Title>
             <Card.Text>
-              Some quick example text to build on the card title and make up
-              the bulk of the card's content.
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
             </Card.Text>
             <Button variant="primary">Go somewhere</Button>
           </Card.Body>
@@ -78,8 +123,8 @@ const Home = () => {
           <Card.Body>
             <Card.Title>Card Title</Card.Title>
             <Card.Text>
-              Some quick example text to build on the card title and make up
-              the bulk of the card's content.
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
             </Card.Text>
             <Button variant="primary">Go somewhere</Button>
           </Card.Body>
@@ -90,8 +135,8 @@ const Home = () => {
           <Card.Body>
             <Card.Title>Card Title</Card.Title>
             <Card.Text>
-              Some quick example text to build on the card title and make up
-              the bulk of the card's content.
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
             </Card.Text>
             <Button variant="primary">Go somewhere</Button>
           </Card.Body>
