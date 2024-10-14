bfeff79880e4dd7d02c1c1ee96345955396f1ee6
MartinBradburyPC <you@example.com>
2024-10-14 17:03:30 +0100

    updated styling of card displaying projects and navbar.

--- mebprofile/src/pages/Home.jsx
+++ mebprofile/src/pages/Home.jsx
@@ -1,74 +1,104 @@
-import React, { Component } from "react";
+import React, { useRef } from "react";
 import styles from "../styles/Home.module.css";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-import Placeholder from "react-bootstrap/Placeholder";
+import video from "../assets/trihub.mp4";
 
-export class Home extends Component {
-  render() {
-    return (
-      <div className={styles.bg}>
-        <div className={styles.cardPos}>
-          <Card className={styles.card}>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-          <Card className={styles.card}>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-          <Card className={styles.card}>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-          <Card className={styles.card}>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-          <Card className={styles.card}>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-          
-        </div>
+
+const Home = () => {
+  const videoRef = useRef(null); // Create a ref for the video element
+
+  const handleMouseEnter = () => {
+    videoRef.current.play(); // Play the video on mouse enter
+  };
+
+  const handleMouseLeave = () => {
+    videoRef.current.pause(); // Pause the video on mouse leave
+    videoRef.current.currentTime = 0; // Reset video to start
+  };
+
+  return (
+    <div className={styles.bg}>
+      <div className={styles.cardPos}>
+        <Card
+          className={styles.card}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+
+        >
+          {/* <Card.Img variant="top" src={image1} className={styles.cardbg}/> */}
+          <video
+            ref={videoRef} // Attach the ref to the video element
+            className={styles.video}
+            muted
+            loop
+            playsInline
+            webkit-playsinline="true"
+            alt="triathlon background video"
+          >
+            <source src={video} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+          <Card.Body className={styles.cardBody}>
+            <Card.Title>Tri Hub</Card.Title>
+            <Card.Text>
+              A Full Stack project using React.js front end and DjangoREST back end.
+            </Card.Text>
+            <Button className={styles.btn}>Link to Project</Button>
+            <Button className={styles.btn}>About</Button>
+          </Card.Body>
+        </Card>
+
+        <Card className={styles.card}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up
+              the bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+
+        <Card className={styles.card}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up
+              the bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+
+        <Card className={styles.card}>
+          <Card.Img variant="top" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up
+              the bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+
+        <Card className={styles.card}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up
+              the bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Home;
